269b88fd1995b52e3501a1e8f16b6fa1e0a4ea49
Forest Olsen <forest.theodore@gmail.com>
2018-03-01 13:55:35 -0500

    pre deployment debug state

--- seed-project/routes/messages.js
+++ seed-project/routes/messages.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+var mongoose = require('mongoose');
 
 var User = require('../models/user');
 var Message = require('../models/messages');
